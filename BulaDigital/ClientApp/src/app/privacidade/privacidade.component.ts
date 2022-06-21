2e37af10a0ee04bed64aa9141ed3548684022f76
Wendel Lemes <wendellemes0@gmail.com>
2022-06-21 14:45:00 -0400

    subindo alterações com: remoção do component fetch e implementando component privacidade

---
+++ BulaDigital/ClientApp/src/app/privacidade/privacidade.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-privacidade',
+  templateUrl: './privacidade.component.html'
+})
+export class PrivacidadeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
