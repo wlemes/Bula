8d3da62f1b0a6bc3dbfbcd8ce8b67f8180e65ead
Wendel Lemes <wendellemes0@gmail.com>
2022-06-20 23:47:08 -0400

    alteração na consulta-categoria

--- BulaDigital/ClientApp/src/app/consulta-categoria/consulta-categoria.component.ts
+++ BulaDigital/ClientApp/src/app/consulta-categoria/consulta-categoria.component.ts
@@ -2,9 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 
 @Component({
-    selector: 'app-consulta-categoria',
-    templateUrl: './consulta-categoria.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-consulta-categoria',
+  templateUrl: './consulta-categoria.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class categoriaComponent {
@@ -16,58 +16,69 @@ export class categoriaComponent {
   reverse = false;
   p: number = 1;
 
-    constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-        this.http = http;
-    }
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.http = http;
+  }
 
-    cabecalho = [
-        { nome: 'Nome', exibe: true, label: 'nomeProduto' },
-        { nome: 'Fabricante', exibe: true, label: 'razaoSocial' },
-        { nome: 'Data de Publicação', exibe: true, label: 'data' },
-        { nome: 'Bula Paciente', exibe: true, label: '' },
-        { nome: 'Bula Médica', exibe: true, label: '' },];
-    
+  cabecalho = [
+    { nome: 'Nome', exibe: true, label: 'nomeProduto' },
+    { nome: 'Fabricante', exibe: true, label: 'razaoSocial' },
+    { nome: 'Data de Publicação', exibe: true, label: 'data' },
+    { nome: 'Bula Paciente', exibe: true, label: '' },
+    { nome: 'Bula Médica', exibe: true, label: '' },];
 
-    listarMedicamentos() {
-        this.http.get<Bula>(`https://bula.vercel.app/medicamentos?categoria=${this.numCategoria}`).subscribe(result => {
-            this.bulas = result.content;
-            this.bulas.length = result.totalElements;
-        }, error => console.error(error));
-    }
 
-    filtroCabecalho(): any[] {
-        return this.cabecalho.filter(cabecalho => cabecalho.exibe);
-    }
+  pagina: number = 1;
+  total: number = 0;
+  itensPorPagina: number = 10;
 
-    sort(key) {
-        this.key = key;
-        this.reverse = !this.reverse;
-    }
+  listarMedicamentos() {
+    this.http.get<Bula>(`https://bula.vercel.app/medicamentos?categoria=${this.numCategoria}&pagina=${this.pagina}`).subscribe(result => {
+      this.bulas = result.content;
+      this.total = result.totalElements;
+      // this.bulas.length = result.totalElements;
+    }, error => console.error(error));
+  }
+
+  filtroCabecalho(): any[] {
+    return this.cabecalho.filter(cabecalho => cabecalho.exibe);
+  }
+
+  sort(key) {
+    this.key = key;
+    this.reverse = !this.reverse;
+  }
+
+  pageChange(page: number) {
+    this.pagina = page;
+    this.listarMedicamentos();
+  }
 }
 
+
 export interface Content {
-    idProduto: number;
-    numeroRegistro: string;
-    nomeProduto: string;
-    expediente: string;
-    razaoSocial: string;
-    cnpj: string;
-    numeroTransacao: string;
-    data: Date;
-    numProcesso: string;
-    idBulaPacienteProtegido: string;
-    idBulaProfissionalProtegido: string;
+  idProduto: number;
+  numeroRegistro: string;
+  nomeProduto: string;
+  expediente: string;
+  razaoSocial: string;
+  cnpj: string;
+  numeroTransacao: string;
+  data: Date;
+  numProcesso: string;
+  idBulaPacienteProtegido: string;
+  idBulaProfissionalProtegido: string;
 }
 
 export interface Bula {
-    content: Content[];
-    totalPages: number;
-    totalElements: number;
-    last: boolean;
-    numberOfElements: number;
-    first: boolean;
-    sort?: any;
-    size: number;
-    number: number;
-  }
+  content: Content[];
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  numberOfElements: number;
+  first: boolean;
+  sort?: any;
+  size: number;
+  number: number;
+}
 
